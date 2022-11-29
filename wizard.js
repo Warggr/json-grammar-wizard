af344b3aa7d9029c6fe72379e3803b41a512438a
Pierre Ballif <pierre.ballif@tum.de>
2022-11-29 22:51:37 +0100

    Read word selected in the DOM

--- wizard.js
+++ wizard.js
@@ -2,18 +2,49 @@ var content = undefined;
 
 var completeGrammar = undefined;
 
-function makeWord(content){
+function assert(truth, message, data){
+	if(! truth) throw new Error(message + data);
+}
+
+function readWord(rootDom){
+	assert(rootDom.classList.contains('spell'), 'rootDom is not a Spell dom element');
+	if(rootDom.classList.contains('literal')){
+		return JSON.parse(rootDom.textContent);
+	} else if(rootDom.classList.contains('list')){
+		let children = [...rootDom.children];
+		return children
+			.filter(elem => elem.classList.contains('spell'))
+			.map(readWord);
+	} else if(rootDom.classList.contains('associative')){
+		let retVal = {};
+		for(let elem of rootDom.children) if(elem.classList.contains('spell')){
+			assert(elem.classList.contains('keyValuePair'));
+			let key = elem.children[0].textContent;
+			let value = readWord(elem.children[1]);
+			retVal[key] = value;
+		}
+		return retVal;
+	} else if(rootDom.classList == 'spell choice') {
+		assert(rootDom.children.length == 2);
+		assert(rootDom.children[0].nodeName == "SELECT");
+		return readWord(rootDom.children[1]);
+	} else {
+		throw new Error('Unrecognized rootDom', rootDom);
+	}
+}
+
+function _makeWord(content){
 	if(content == null){
 		throw new Error('Literal null matches nothing');
 	} else if(typeof content === 'number'){
 		throw new Error('Number literal matches nothing: ', content);
 	} else if(typeof content === 'string'){
 		if(!completeGrammar.hasOwnProperty(content)) throw new Error('Varname ' + content + ' not found');
-		else return makeWord(completeGrammar[content]);
+		else return _makeWord(completeGrammar[content]);
 	} else if(Array.isArray(content)){
 		if(content.length >= 2){ // "or"
 			let rootDom = document.createElement('div');
-			rootDom.classList = 'wizard choice';
+			rootDom.classList = 'spell choice';
 			let selector = document.createElement('select');
 			rootDom.appendChild(selector);
 			for(let poss of content){
@@ -24,17 +55,17 @@ function makeWord(content){
 			selector.onchange = event => {
 				let selector = event.srcElement; let parent = selector.parentNode;
 				parent.removeChild(parent.children[1]);
-				parent.appendChild(makeWord(JSON.parse(selector.selectedOptions[0].textContent)));
+				parent.appendChild(_makeWord(JSON.parse(selector.selectedOptions[0].textContent)));
 			};
-			rootDom.appendChild(makeWord(content[0]));
+			rootDom.appendChild(_makeWord(content[0]));
 			return rootDom;
 		} else if(content.length == 1){ // literal
 			let literal = content[0];
 			if(literal == null || typeof literal == 'number' || typeof literal == 'string'){
-				let retVal = document.createElement('p'); retVal.textContent = literal; return retVal;
+				let retVal = document.createElement('p'); retVal.classList = 'spell literal'; retVal.textContent = JSON.stringify(literal); return retVal;
 			} else if(Array.isArray(literal)){
 				let rootDom = document.createElement('div');
-				rootDom.classList = 'wizard literal array list';
+				rootDom.classList = 'spell array list';
 				let open = document.createElement('span'); open.classList = 'bracket'; open.textContent = '['; rootDom.appendChild(open);
 				let comma;
 				if(literal.length == 1){
@@ -43,7 +74,7 @@ function makeWord(content){
 					let length = literal[1];
 					if(typeof length != 'number') throw new Error('Expected second element of array to be a number');
 					for(let i = 0; i<length; i++){
-						rootDom.appendChild(makeWord(literal[0]));
+						rootDom.appendChild(_makeWord(literal[0]));
 						comma = document.createElement('span'); comma.textContent = ','; rootDom.appendChild(comma);
 					}
 				} else {
@@ -53,15 +84,14 @@ function makeWord(content){
 				return rootDom;
 			} else if(typeof literal == 'object'){
 				let rootDom = document.createElement('div');
-				rootDom.classList = 'wizard literal array associative';
+				rootDom.classList = 'spell array associative';
 				let open = document.createElement('span'); open.classList = 'bracket'; open.textContent = '{'; rootDom.appendChild(open);
 				let comma;
-				console.log(literal);
 				for(let key in literal){
-					console.log(key, literal[key]);
 					let keyValuePairDom = document.createElement('div');
-					let name = document.createElement('span'); name.textContent = key + ":"; keyValuePairDom.appendChild(name);
-					keyValuePairDom.appendChild(makeWord(literal[key]));
+					keyValuePairDom.classList = 'spell keyValuePair';
+					let name = document.createElement('span'); name.textContent = key; keyValuePairDom.appendChild(name);
+					keyValuePairDom.appendChild(_makeWord(literal[key]));
 					rootDom.appendChild(keyValuePairDom);
 					comma = document.createElement('span'); comma.textContent = ','; rootDom.appendChild(comma);
 				}
@@ -73,10 +103,13 @@ function makeWord(content){
 		}
 	} else if(typeof content === 'object'){
 		throw new Error('Literal dicts should be enclosed in lists - this one isn\'t: ' + JSON.stringify(content));
+	} else {
+		console.warn(content);
+		throw new Error('Object not recognized by grammar');
 	}
 }
 
-function enwizen(grammar){
+function makeWord(grammar){
 	completeGrammar = grammar;
-	return makeWord(grammar.root);
+	return _makeWord(grammar.root);
 }
